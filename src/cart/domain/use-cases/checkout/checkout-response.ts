4b30208c7d7406f2485024025f259f4c2bc6f9a2
Thiago Fernandes <fernandes.thiagoc@gmail.com>
2021-08-30 19:35:35 -0300

    refact: change checkout response property to attribute
    The totalAmountWithDiscount property has been changed to
    an attribute for easy conversion to json object for response.
    
    Signed-off-by: Thiago Fernandes <fernandes.thiagoc@gmail.com>

--- src/cart/domain/use-cases/checkout/checkout-response.ts
+++ src/cart/domain/use-cases/checkout/checkout-response.ts
@@ -4,15 +4,13 @@ import { CheckoutResponseProductInterface } from '../../contracts/checkout-respo
 export class CheckoutResponse implements CheckoutResponseInterface {
   totalAmount = 0;
   totalDiscount = 0;
+  totalAmountWithDiscount = 0;
   products: Array<CheckoutResponseProductInterface> = [];
 
   addProduct(product: CheckoutResponseProductInterface) {
     this.products.push(product);
     this.totalAmount += product.totalAmount;
     this.totalDiscount += product.discount;
-  }
-
-  get totalAmountWithDiscount(): number {
-    return this.totalAmount - this.totalDiscount;
+    this.totalAmountWithDiscount = this.totalAmount - this.totalDiscount;
   }
 }
