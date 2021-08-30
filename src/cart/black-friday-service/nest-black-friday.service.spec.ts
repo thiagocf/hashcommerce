6dc78c57cde801d1b6bbd603f2be568459948e70
Thiago Fernandes <fernandes.thiagoc@gmail.com>
2021-08-30 19:30:32 -0300

    test: nest black friday service DI fix
    Includes the ProductRespositoryService as dependency of
    NestBlackFridayService for test execution.
    
    Signed-off-by: Thiago Fernandes <fernandes.thiagoc@gmail.com>

--- src/cart/black-friday-service/nest-black-friday.service.spec.ts
+++ src/cart/black-friday-service/nest-black-friday.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NestBlackFridayService } from './nest-black-friday.service';
+import { ProductRepositoryService } from '../product-repository/product-repository.service';
 
 describe('nest black friday service', () => {
   let service: NestBlackFridayService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NestBlackFridayService],
+      providers: [ProductRepositoryService, NestBlackFridayService],
     }).compile();
 
     service = module.get<NestBlackFridayService>(NestBlackFridayService);
